27309acfbd7cbd975df82eeb721d07b15f17ef8c
Irfan <assidiq.irfan@gmail.com>
2020-09-04 23:39:14 +0700

    add detailMBGActivity

--- app/src/main/java/com/irfan/project/testmobile/activities/DetailMBGActivity.kt
+++ app/src/main/java/com/irfan/project/testmobile/activities/DetailMBGActivity.kt
@@ -1,10 +1,16 @@
 package com.irfan.project.testmobile.activities
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 /**
  *   created by Irfan Assidiq
  *   email : assidiq.irfan@gmail.com
  *   class for detail movies by genre activity
  **/
-class DetailMBGActivity {
+class DetailMBGActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
